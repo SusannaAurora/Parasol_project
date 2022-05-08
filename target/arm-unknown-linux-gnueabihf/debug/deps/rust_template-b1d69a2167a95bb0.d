9c9b0cb9c58dc68163577705b86a9cee7cd6a1e9
SusannaAurora <70034586+SusannaAurora@users.noreply.github.com>
2022-05-08 17:56:06 +0200

    Delete target directory

--- target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0.d
+++
@@ -1,8 +0,0 @@
-/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0: src/main.rs src/parasol.rs src/myservo.rs src/stepper.rs
-
-/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0.d: src/main.rs src/parasol.rs src/myservo.rs src/stepper.rs
-
-src/main.rs:
-src/parasol.rs:
-src/myservo.rs:
-src/stepper.rs:
--- target/debug/build/libc-793ff3e9545f5632/build_script_build-793ff3e9545f5632.d
+++
@@ -1,5 +0,0 @@
-/home/susannaaurora/Projects/final-project-project-team-5/target/debug/build/libc-793ff3e9545f5632/build_script_build-793ff3e9545f5632: /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.76/build.rs
-
-/home/susannaaurora/Projects/final-project-project-team-5/target/debug/build/libc-793ff3e9545f5632/build_script_build-793ff3e9545f5632.d: /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.76/build.rs
-
-/home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.76/build.rs:
