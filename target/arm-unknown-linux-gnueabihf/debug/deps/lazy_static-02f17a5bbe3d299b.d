540ea274ae88882832817986e0ecd649051dbede
SusannaAurora <70034586+SusannaAurora@users.noreply.github.com>
2022-05-08 15:31:48 +0000

    Copying all to personal github

---
+++ target/arm-unknown-linux-gnueabihf/debug/deps/lazy_static-02f17a5bbe3d299b.d
@@ -0,0 +1,8 @@
+/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/lazy_static-02f17a5bbe3d299b.rmeta: /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/liblazy_static-02f17a5bbe3d299b.rlib: /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/lazy_static-02f17a5bbe3d299b.d: /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
+/home/susannaaurora/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
---
+++ target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0.d
@@ -0,0 +1,8 @@
+/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0: src/main.rs src/parasol.rs src/myservo.rs src/stepper.rs
+
+/home/susannaaurora/Projects/final-project-project-team-5/target/arm-unknown-linux-gnueabihf/debug/deps/rust_template-b1d69a2167a95bb0.d: src/main.rs src/parasol.rs src/myservo.rs src/stepper.rs
+
+src/main.rs:
+src/parasol.rs:
+src/myservo.rs:
+src/stepper.rs:
